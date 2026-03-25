c31dd838f58612707a3d91ae9d7b83363a677f34
Hemedi itongwa <hemed.itongwa90@gmail.com>
2026-03-25 10:20:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const app = express();
 app.use(express.json());
 
 const userRoutes = require('./routes/userRoutes');
-const productRoutes = require('./routes/productRoutes');
+const produitRoutes = require('./routes/produitRoutes');
 
 app.use('/users', userRoutes);
-app.use('/products', productRoutes);
+app.use('/products', produitRoutes);
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
